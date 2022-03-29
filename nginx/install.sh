347fa9a2b000ec33910e3657b167428582649691
max <max@ntc.local>
2022-03-29 19:27:12 +0300

    add install script

---
+++ nginx/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo yum install epel-release
+sudo yum update
+sudo yum install -y nginx
--- nginx/main.tf
+++ nginx/main.tf
@@ -14,3 +14,19 @@ provider "aws" {
     }
   }
 }
+
+resource "aws_instance" "nginx" {
+  ami = 
+  instance_type = "t2.micro"
+  key_name = "nginx"
+}
+
+data "template_file" "install_nginx" {
+  template = file("./install.sh")
+}
+
+resource "aws_instance" "nginx" {
+  ami           = "ami-0dcc0ebde7b2e00db"
+  instance_type = "t2.micro"
+  user_data = data.template_file.install_nginx.rendered
+}
